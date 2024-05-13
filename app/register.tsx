4a9e40e1610af5b250e51a90754e7c730c4e7ea9
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-13 17:51:57 +0100

    added toggle password visibility to the input field

--- app/register.tsx
+++ app/register.tsx
@@ -1,14 +1,37 @@
-import { AuthInputField } from '@/components'
-import React from 'react'
+import { AuthInputField, PasswordVisibilityIcon } from '@/components'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 const register = () => {
+  const [secureTextEntry, setSecureTextEntry] = useState<boolean>(false)
   return (
-    <View style={styles.container} >
+    <View style={styles.container}>
       <Text>register</Text>
-      <AuthInputField name='name' placeholder='Charles Tabot' label='Name' containerStyle={{marginBottom:16}} />
+      <AuthInputField
+        name="name"
+        placeholder="Charles Tabot"
+        label="Name"
+        containerStyle={{ marginBottom: 16 }}
+      />
+      <AuthInputField
+        name="email"
+        placeholder="ebezebeatrice@gmail.com"
+        label="Email Address"
+        containerStyle={{ marginBottom: 16 }}
+      />
+      <AuthInputField
+        name="password"
+        placeholder="*************"
+        label="Password"
+        containerStyle={{ marginBottom: 16 }}
+        secureTextEntry={!secureTextEntry}
+        rightIcon={<PasswordVisibilityIcon privateIcon={secureTextEntry} />}
+        onRightIconPress={() => {
+          setSecureTextEntry(!secureTextEntry)
+        }}
+      />
     </View>
-  )
+  );
 }
 
 export default register
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,7 @@
 const PRIMARY = "#443333";
 const SECONDARY = "#EEA849";
-const CONTRAST = "#FFFFFF";
-const INACTIVE_CONTRAST = "rgba(255, 255, 255, 0.5)";
+const CONTRAST = "green";
+const INACTIVE_CONTRAST = "green";
 const ERROR = "#FF5B14";
 const SUCCESS = "#3BEF1E";
 const OVERLAY = "rgba(45, 35, 35, 0.5)";
