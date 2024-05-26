683ccb91666823e7321bc2d25119c401fe6de292
TabotCharlesBessong <ebezebeatrice@gmail.com>
2024-05-26 18:19:17 +0100

    notifications card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,15 +2,40 @@ import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import { Notificationcard } from "@/components";
+import { StatusBar } from "expo-status-bar";
 
 const index = () => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.push("register")} >
-      <Notificationcard />
-      <Text>Home Alone</Text>
+      <TouchableOpacity onPress={() => router.push("register")}>
+        <Notificationcard
+          image="../../assets/images/doctor.jpeg"
+          title="Upcoming appointment with Dr Smith"
+          subject="You want to talk about your gastritis situation"
+          time="11:15 AM"
+        />
+        <Notificationcard
+          image="../../assets/images/doctor.jpeg"
+          title="Upcoming appointment with Dr Smith"
+          subject="You want to talk about your gastritis situation"
+          time="10:30 AM"
+        />
+        <Notificationcard
+          image="../../assets/images/doctor.jpeg"
+          title="Visiting the lab technician today"
+          subject="You want to talk about your gastritis situation"
+          time="2:45 PM"
+        />
+        <Notificationcard
+          image="../../assets/images/doctor.jpeg"
+          title="Upcoming appointment with Dr Smith"
+          subject="You want to talk about your gastritis situation"
+          time="11:15 AM"
+        />
+        <Text>Home Alone</Text>
       </TouchableOpacity>
+      <StatusBar style="auto" />
     </View>
   );
 };
@@ -20,8 +45,9 @@ export default index;
 const styles = StyleSheet.create({
   container: {
     display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
     flex: 1,
+    // width:"100%"
   },
 });
