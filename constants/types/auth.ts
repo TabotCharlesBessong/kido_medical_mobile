d9120be9beabcaad999bc2d50890dfd1bea96316
TabotCharlesBessong <ebezebeatrice@gmail.com>
2025-07-10 10:42:44 +0100

    starting with types

---
+++ constants/types/auth.ts
@@ -0,0 +1,27 @@
+export interface RegisterValues {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginValues {
+  email: string;
+  password: string;
+}
+
+export interface VerifyValues {
+  email: string;
+  otp: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: {
+    id: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    role: string;
+  };
+}
